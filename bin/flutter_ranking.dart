c8b6ffce5c7192acf42886ee645315ad5d815305
myConsciousness <contact@shinyakato.dev>
2022-12-17 19:35:38 +0900

    update

--- bin/flutter_ranking.dart
+++ bin/flutter_ranking.dart
@@ -49,7 +49,7 @@ Future<void> _writeHistoryFile(
         ..addValue(package.version)
         ..addValue('${package.popularity}')
         ..addValue('${package.likeCount}')
-        ..addValue('${package.likeCount}')
+        ..addValue('${package.starCount}')
         ..addValue('${package.forkCount}')
         ..addValue('${package.issueCount}')
         ..addValue(package.owner)
@@ -79,13 +79,13 @@ Future<void> _writeReadmeFile(
 
 I respect all OSS developers who are developing great packages! 🫡
 
-This project aims to visualize the ranking of OSS packages published on pub.dev to further invigorate the community and promote competition.
+This project aims to visualize the ranking of OSS packages published on [pub.dev](https://pub.dev) to further invigorate the community and promote competition.
 
 Currently, the top 3,000 packages are listed in this ranking, sorted by popularity index as evaluated by pub.dev. This process is fully automated and is recalculated daily at 12:00 and 24:00 UTC time.
 
-**Show some ❤️ and star the repo to support the project！**
+**Show some ❤️ and star the repo to support the project!**
 
-## Last Updated: ${now.toIso8601String()}
+## Last Updated (UTC): ${now.toUtc().toIso8601String()}
 
 ''');
 
@@ -130,19 +130,29 @@ Currently, the top 3,000 packages are listed in this ranking, sorted by populari
     _write(
       readmeFile,
       TableRecord()
-        ..addValue('$rank')
-        ..addValue(package.name)
+        ..addValue('**$rank**')
+        ..addValue(
+            '[${package.name}](https://pub.dev/packages/${package.name})')
         ..addValue(package.description)
-        ..addValue(package.version)
-        ..addValue('${package.popularity}')
-        ..addValue('${package.likeCount}')
-        ..addValue('${package.likeCount}')
-        ..addValue('${package.forkCount}')
-        ..addValue('${package.issueCount}')
-        ..addValue(package.owner)
-        ..addValue(package.publisher)
-        ..addValue(package.license)
-        ..addValue(package.updatedAt.toIso8601String()),
+        ..addValue(
+            '[${package.version}](https://pub.dev/packages/${package.name}/versions)')
+        ..addValue(
+            '![Popularity](https://img.shields.io/pub/popularity/${package.name}?label=Pub%20Popularity&style=fflat-squaree)')
+        ..addValue(
+            '![Likes](https://img.shields.io/pub/likes/${package.name}?label=Pub%20Likes&style=fflat-squaree)')
+        ..addValue(
+            '[![Stars](https://img.shields.io/github/stars/${package.owner}/${package.name}?logo=github&logoColor=white)](https://github.com/${package.owner}/${package.name})')
+        ..addValue(
+            '[![Forks](https://img.shields.io/github/forks/${package.owner}/${package.name}?logo=github&logoColor=white)](https://github.com/${package.owner}/${package.name})')
+        ..addValue(
+            '[![Issues](https://img.shields.io/github/issues/${package.owner}/${package.name}?logo=github&logoColor=white)](https://github.com/${package.owner}/${package.name})')
+        ..addValue('[@${package.owner}](https://github.com/${package.owner})')
+        ..addValue(
+            '[${package.publisher}](https://pub.dev/publishers/${package.publisher}/packages)')
+        ..addValue(
+            '[![License](https://img.shields.io/github/license/${package.owner}/${package.name}?logo=open-source-initiative&logoColor=green)](https://github.com/${package.owner}/${package.name}/blob/main/LICENSE)')
+        ..addValue(
+            '[![Last Commits](https://img.shields.io/github/last-commit/${package.owner}/${package.name}?logo=git&logoColor=white)](https://github.com/${package.owner}/${package.name}/commits/main)'),
     );
 
     rank++;

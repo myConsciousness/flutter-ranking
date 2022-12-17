269146447c630149f02c3324ac2393539a7ac0e8
myConsciousness <contact@shinyakato.dev>
2022-12-17 21:34:34 +0900

    update

--- bin/flutter_ranking.dart
+++ bin/flutter_ranking.dart
@@ -81,14 +81,14 @@ I respect all OSS developers who are developing great packages! 🫡
 
 This project aims to visualize the ranking of OSS packages published on [pub.dev](https://pub.dev) to further invigorate the community and promote competition.
 
-Currently, the top 3,000 packages are listed in this ranking, sorted by popularity index as evaluated by [pub.dev](https://pub.dev). This process is fully automated and ranking is recalculated daily per 4 hours based on UTC time ✨
+Currently, the top 3,000 packages are listed in this ranking, sorted by popularity index as evaluated by [pub.dev](https://pub.dev). This process is fully automated and ranking is recalculated daily per 3 hours based on UTC time ✨
 
 **Show some ❤️ and star the repo to support the project!**
 
 > **Notification:**</br>
 > Last Updated (UTC): ${now.toUtc().toIso8601String()}
 
-## Content
+## Content 🎉
 
 ''');
 

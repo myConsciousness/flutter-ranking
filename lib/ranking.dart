15c7774e1a2f63f0cf7f7b19201bcc16ab7dcb7c
myConsciousness <contact@shinyakato.dev>
2022-12-17 17:21:23 +0900

    update

--- lib/ranking.dart
+++ lib/ranking.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart';
 import 'package:pub_api_client/pub_api_client.dart';
 
 final pub = PubClient();
-const _bearerToken = String.fromEnvironment('BEARER_TOKEN');
+final _bearerToken = Platform.environment['BEARER_TOKEN'];
 
 const _maxCount = 5;
 
